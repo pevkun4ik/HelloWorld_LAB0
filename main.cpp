2690d43eac749c8f5474b474b3272a72201b5051
pevkun4ik <bogsav10@mail.ru>
2025-09-04 13:18:12 +0300

    Savinskii_AS2404

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void f() {
+    int c;
+    cin >> c;
+    cout << "Вы ввели: " << c << endl;
+}
+
 int main() {
     cout << "Hello world" << endl;
     cout << "Введите символ: ";
-    char c;
-    cin >> c;
-    cout << "Вы ввели: " << c << endl;
+    f();
     return 0;
 }

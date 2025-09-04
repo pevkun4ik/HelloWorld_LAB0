3b4681214f11215b201f1fe4e81df8d4745735d0
pevkun4ik <bogsav10@mail.ru>
2025-09-04 12:44:47 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world" << endl;
+    cout << "Введите символ: ";
+    char c;
+    cin >> c;
+    cout << "Вы ввели: " << c << endl;
+    return 0;
+}
